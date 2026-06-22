f2d5abb1d0409baa69d517a0ed1ee3bf26052b3e
Pan1ka812 <dimazinchuk12@gmail.com>
2026-06-22 20:04:22 +0300

    fix: temp hardcode new bot token

--- bot.py
+++ bot.py
@@ -35,7 +35,7 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
 # =========================
 # CONFIG
 # =========================
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7394588586:AAE3Xxr7mNIhJejvQ4jTUhUs-W6Hu9b1gGg")
 ADMIN_CHAT_ID = int(os.getenv("ADMIN_CHAT_ID", "0"))
 REVIEWS_CHAT_ID = int(os.getenv("REVIEWS_CHAT_ID", "0"))
 SUPPORT_PHONE = os.getenv("SUPPORT_PHONE", "")
